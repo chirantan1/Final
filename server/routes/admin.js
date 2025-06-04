263279095b2184eed17a787fa6121c3c8e8675dc
Chirantan Jana <97895958+chirantan1@users.noreply.github.com>
2025-06-04 23:55:10 +0530

    Delete server/routes/admin.js

--- server/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example admin route
-router.get('/', (req, res) => {
-  res.json({ success: true, message: 'Admin route' });
-});
-
-module.exports = router;
